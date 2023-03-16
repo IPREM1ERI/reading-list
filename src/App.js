ff76205cd9550470a3d0602239e0c46173b5cfc5
davidv <davidvazquezgit@gmail.com>
2023-03-16 13:51:15 -0700

    work on BookCreate component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('Book title:', title)
+  };
+
+  const editBook = () => {
+
+  };
+
+  const deleteBook = () => {
+
+  };
+
+
   return (
     <div>
-      App
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,5 +1,25 @@
-function BookCreate() {
-  return <div>BookCreate</div>
+import { useState } from "react"
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('')
+
+  const handleChange = (evt) => {
+    setTitle(evt.target.value)
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    onCreate(title)
+  }
+
+  return (
+    <form>
+      <label>Title</label>
+      <input  value={title} onChange={handleChange} />
+      <button onSubmit={handleSubmit}>Create!</button>
+    </form>
+    )
+    
 }
 
 export default BookCreate
